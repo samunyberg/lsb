38061346bc9a870e94ae4a2078ac955543436f8a
samunyberg <samu.nyberg@gmail.com>
2025-01-02 19:26:14 +0200

    Fix failed migrations

--- src/prisma/migrations/20250102144123_change_admin_details_to_admin_note_in_user_model/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `adminDetails` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `adminDetails`,
-    ADD COLUMN `adminNote` VARCHAR(500) NULL;
